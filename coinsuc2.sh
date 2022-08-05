097048dfd749c77e220b92428c1e4e464c26e4d8
surajbridgelabz <surajnsinha@gmail.com>
2022-08-05 20:26:41 +0530

    days10assignment

---
+++ coinsuc2.sh
@@ -0,0 +1,20 @@
+randomCheck=$((RANDOM))
+choice =input("pick heads (h) or tails (t): ")
+num = randrange(0,2)
+if num == 0;
+coin = "h"
+then
+echo print("it was head!")
+elif
+coin = "t"
+then
+echo print("it was tails!")
+if [coin == choice]
+then
+echo print("you win!")
+elif
+echo print("you lost!")
+fi
+fi
+fi
+fi
